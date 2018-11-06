9a48c69a76cb1c38ffabd525bedd82c4304c34cc
Patrick McCaffrey <allknowingq@gmail.com>
2018-11-06 18:34:37 -0500

    Surface - Moved key generation to its own method

--- src/main/java/com/mycompany/bitconv2/Main.java
+++ src/main/java/com/mycompany/bitconv2/Main.java
@@ -17,17 +17,32 @@ import org.bitcoinj.params.MainNetParams;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Program BitConV2 Start");
+        System.out.println("Program BitConV2 start");
+        
+        String[] tempKeyPair =  keyGenerator();
+        
+        System.out.println("Values Returned: " + tempKeyPair[0] + " and " + tempKeyPair[1]);
+        
+        System.out.println("Program finished");
+    }
+    
+    private static String[] keyGenerator() {
+        System.out.println("keyGenerator starting");
         
         NetworkParameters params = MainNetParams.get();
         String filePrefix = "forwarding-service";
         
         SecureRandom random = new SecureRandom();
-        
         ECKey key = new ECKey(random);
         
-        System.out.println("Private Key: " + key.getPrivateKeyAsHex());
-        System.out.println("Adress: " + key.toAddress(params));
-        System.out.println("Program Finished");
+        String[] returnValue = new String[2];
+        returnValue[0] = key.getPrivateKeyAsHex();
+        returnValue[1] = "" + key.toAddress(params);
+        
+        System.out.println("Private Key: " + returnValue[0]);
+        System.out.println("Adress: " + returnValue[1]);
+        
+        System.out.println("keyGenerator finished");
+        return returnValue;
     }
 }
