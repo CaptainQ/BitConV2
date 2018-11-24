0672de2c4f9fb8bae69490f6590aeb7e5ea8d0b5
Patrick McCaffrey <allknowingq@gmail.com>
2018-11-24 04:23:12 -0500

    Surface - Fixed NeuralNet creation code in Main

--- src/main/java/com/mycompany/bitconv2/Main.java
+++ src/main/java/com/mycompany/bitconv2/Main.java
@@ -38,7 +38,7 @@ public class Main {
         
         System.out.println("Program finished");
         
-        NeuralNet net = new NeuralNet();
+        NeuralNet net = NeuralNet.getNeuralNet();
     }
     
     private static void generateTrainingData() throws IOException {
